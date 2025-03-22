4685ae9967fc1feb291736589935d1484888050c
haksang <berzodoger@naver.com>
2025-03-22 13:34:51 +0900

    응애

---
+++ mylife/src/calendar/Callcalendar.java
@@ -0,0 +1,55 @@
+package calendar;
+
+import java.util.Scanner; //Scanner: 입력 받을수 있음
+import java.time.LocalDate; // LocalDate date = LocalDate.of(YYYY,MM,DD);
+import java.time.LocalTime; // LocalTime time = LocalTime.of(hh,mm,ss);
+import java.time.LocalDateTime; // LocalDateTime datetime = LocalDateTime.of(YYYY,MM,DD,hh,mm,ss);
+
+public class Callcalendar {
+	
+	static void notint() { //아무것도 안 적으면 default, 같은 패키지 내에서만 사용 가능, private는 해당 클래스에서만 사용 가능
+		System.out.print("올바르지 않은 값 ('YYYY MM DD' 형태로 입력하세요)");
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in); //System: 표준 입출력 기능, in: 입력
+		
+		LocalDate today = LocalDate.now(); //현재 날짜 가져오기
+		
+		boolean t=true;
+		
+		int y = today.getYear(), m = today.getMonthValue(), d = today.getDayOfMonth();
+		String[] yyyymmdd = new String[3];
+		
+		while (t) {
+			System.out.print("\n\n\n\n\n");
+			MainCalendar.dayprint(y, m, d); //기본 달력 출력
+			
+			while (true) {
+				System.out.print("\n");
+				String inputdate = scn.nextLine();
+				try {
+					yyyymmdd = inputdate.split(" "); // inputdate를 공백을 기준으로 split(분리) 하여 배열에 저장
+					y = Integer.parseInt(yyyymmdd[0]); //Integer.parseInt : 정수변환 
+					m = Integer.parseInt(yyyymmdd[1]);
+					d = Integer.parseInt(yyyymmdd[2]);
+					if (m>=13) {
+						notint();
+						continue;
+					}
+					if (d>=32 || (d==31 && MainCalendar.m31or30(y, m)<=30) || (d==30 && m==2 && MainCalendar.yoonyear(y)) || (d==29 && m==2 && !MainCalendar.yoonyear(y))) {
+						notint();
+						continue;
+					}
+					break;
+				} catch (Exception e){
+					notint();
+				}
+			}
+			
+			
+		}
+
+	}
+
+}
---
+++ mylife/src/calendar/MainCalendar.java
@@ -0,0 +1,73 @@
+package calendar;
+
+
+
+public class MainCalendar {
+	
+	//윤년 계산
+	public static boolean yoonyear(int year) {
+		boolean yoon = false;
+		if ((year%4==0 && year%100!=0) || year%400==0) {
+			yoon = true;
+		}
+		return yoon;
+	}
+	
+	//해당 달이 몇일인지 계산
+	public static int m31or30(int y, int m) {
+		int tmp=0;
+		
+		if (m==2) {
+			tmp = 28+(yoonyear(y) ? 1 : 0); //+연산이 삼항연산보다 우선이라 괄호필요 
+		}	
+		else if(m<8)
+			tmp = (m%2==1) ? 31 : 30;
+		else
+			tmp = (m%2==0) ? 31 : 30;
+		
+		return tmp;
+	}
+	
+	//해당 달 첫날 요일 계산
+	public static int dayoftheweek(int y, int m) {
+		int yoil = 0; //1일때 월요일
+		int sum = 0; //1년 1월 1일부터 총 일 수
+		
+		for (int i = 1 ; i < y ; i++) {
+			sum = sum+(yoonyear(i) ? 366 : 365);
+		}
+		for (int j = 1 ; j < m ; j++) {
+			sum = sum+(m31or30(y,j));
+		}
+		
+		yoil = sum % 7+1; //7로 나눈 나머지+1이 다음달 첫날 요일
+		return yoil;
+	}
+	
+	
+	public static void dayprint(int y, int m, int d) {
+		System.out.printf("\t\t%d년 \t%d월\n",y,m);
+		System.out.printf("일\t월\t화\t수\t목\t금\t토\n"); //\t는 tap
+		
+		int firstday = dayoftheweek(y,m), lastday = m31or30(y,m);
+		
+		//1일 요일 기준으로 빈공간 만들기
+		if (firstday != 7) {
+			for(int i = 0 ; i < firstday ; i++)
+				System.out.print("\t");
+		}
+		
+		for(int j = 0 ; j < lastday ; j++) {
+			if (j+1==d) {
+				System.out.printf("[%d]\t",j+1);
+			} else {
+				System.out.printf("%d\t",j+1);
+			}
+			if (firstday%7 == 6) // 토요일일때
+				System.out.print("\n");
+			
+			firstday++;
+		}
+	}
+
+}
