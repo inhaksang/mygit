bafa0e4a436994ad414a86f65c8f12c437ad0dcd
haksang <berzodoger@naver.com>
2025-03-26 23:22:58 +0900

    현재시간

--- mylife/src/clock/Clocknow.java
+++ mylife/src/clock/Clocknow.java
@@ -7,21 +7,23 @@ public class Clocknow {
 
 	public static void main(String[] args) {
 		LocalTime timenow = LocalTime.now();
-		DateTimeFormatter form = DateTimeFormatter.ofPattern("hh:mm:ss"); //시:분:초 형태로 hh는 12시간형태
-		String strtime = timenow.format(form);
+		//DateTimeFormatter form = DateTimeFormatter.ofPattern("hh:mm:ss"); 시:분:초 형태로 hh는 12시간형태
+		//String strtime = timenow.format(form);
 		
-		int nowH = timenow.getHour();
-		int nowM = timenow.getMinute();
-		int nowS = timenow.getSecond();
+		String apm;
 		
-		LocalTime gettime = LocalTime.of(8, 58, 58);
-		int getH = gettime.getHour();
-		String now0H = String.format("%02d", nowH); // %03d는 최소 3자리, 빈 공간은 0으로 채운다는 의미
+		int nowH = timenow.getHour(); //timenow의 결과에서 시간만 입력
+		int nowM = timenow.getMinute(); //분 만 입력
+		int nowS = timenow.getSecond(); //초 만 입력
 		
-		System.out.printf("%02d시 %02d분 %02d초 \n", nowH, nowM, nowS);
-		//System.out.printf("%02d시",getH);
+		if (nowH >=13) {
+			nowH -=12;
+			apm = "오후";
+		} else {
+			apm = "오전";
+		}
 		
-
+		System.out.printf("%s %02d시 %02d분 %02d초 \n", apm, nowH, nowM, nowS);
 	}
 
 }
