8c5e86971bbce5225e238068655671bbf59c3620
naeunhwa <new0804@gmail.com>
2025-03-28 22:54:16 +0900

    마저 수정

--- mylife/src/clock/Clocknow.java
+++ mylife/src/clock/Clocknow.java
@@ -1,29 +1,55 @@
 package clock;
+import java.util.Scanner;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Clocknow {
+	public static int nowH;
+	public static int nowM;
+	public static int nowS;
+	
+	public static void timenow() {
+		
+		LocalTime timenow = LocalTime.now();
+		nowH = timenow.getHour(); //timenow의 결과에서 시간만 입력
+		nowM = timenow.getMinute(); //분 만 입력
+		nowS = timenow.getSecond(); //초 만 입력
+	}
 	
 
 	public static void main(String[] args) {
-		LocalTime timenow = LocalTime.now();
-		//DateTimeFormatter form = DateTimeFormatter.ofPattern("hh:mm:ss"); 시:분:초 형태로 hh는 12시간형태
-		//String strtime = timenow.format(form);
+		
+		timenow();
+		
 		
 		String apm;
-		
-		int nowH = timenow.getHour(); //timenow의 결과에서 시간만 입력
-		int nowM = timenow.getMinute(); //분 만 입력
-		int nowS = timenow.getSecond(); //초 만 입력
+		int nowh;
 		
 		if (nowH >=13) {
-			nowH -=12;
+			nowh = nowH-12;
 			apm = "오후";
 		} else {
+			nowh = nowH;
 			apm = "오전";
 		}
 		
-		System.out.printf("%s %02d시 %02d분 %02d초 \n", apm, nowH, nowM, nowS);
+		System.out.printf("%s %02d시 %02d분 %02d초 \n", apm, nowh, nowM, nowS);
+		
+		
+		Scanner scntimer = new Scanner(System.in);
+		
+		try {
+			int timerH = scntimer.nextInt();
+			int timerM = scntimer.nextInt();
+			int timerS = scntimer.nextInt();
+			
+			Settimer.Settimer(timerH, timerM, timerS);
+			
+			scntimer.close();
+		} catch(Exception e) {
+			System.out.println("시간 형태가 잘못되었습니다. 00 00 00 형태로 입력하세요");
+			
+		}
 	}
 
 }
--- mylife/src/clock/Settimer.java
+++ mylife/src/clock/Settimer.java
@@ -8,17 +8,43 @@ import java.util.Timer;
 
 public class Settimer {
 
-	public static void main(String[] args) {
+	public static void Settimer(int h, int m, int s) {
 		Timer t = new Timer();
+	
+		
+		Clocknow.timenow(); //nowH, nowM, nowS 얻기
+		LocalTime scntime = LocalTime.of(h,m,s);//타이머 설정 시간
+		int scnH = scntime.getHour();
+		int scnM = scntime.getMinute();
+		int scnS = scntime.getSecond();
+		
+		
+		int targetH = Clocknow.nowH + scnH;
+		int targetM = Clocknow.nowM + scnM;
+		int targetS = Clocknow.nowS + scnS;
+		
+		int targeth;
+		String apm;
+		if (targetH >=13) {
+			targeth = targetH-12;
+			apm = "오후";
+		} else {
+			targeth = targetH;
+			apm = "오전";
+		}
+		
+		int targetTime = 1000*(scnS + 60*(scnM + 60*(scnH))); //ms 단위
+		
+		
+				
 		TimerTask task = new TimerTask() {
-			
 			public void run() {
-				System.out.println("안녕");
+				System.out.println("응애");
 			}
 		};
-		
-		
-		t.schedule(task, 3000); // schedule(task를 , ~ms마다 수행);
+		System.out.printf("%s %02d시 %02d분 %02d초.\n", apm, nowh, targetM, targetS);
+		System.out.printf("%s %02d시 %02d분 %02d초에 알림이 울립니다.\n", apm, targeth, targetM, targetS);
+		t.schedule(task, targetTime); // schedule(task를 , ~ms후 수행);
 		
 		
 	}
